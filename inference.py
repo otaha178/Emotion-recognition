9c0de797a8b182c9aaf01d3cc2f9c5caccf6dedc
omar178 <31745935+omar178@users.noreply.github.com>
2018-08-25 08:31:54 +0200

    Delete inference.py

--- inference.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-\
-def get_colors(num_classes):
-    colors = plt.cm.hsv(np.linspace(0, 1, num_classes)).tolist()
-    colors = np.asarray(colors) * 255
-    return colors
-
