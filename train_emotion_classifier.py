6ba6bee77f1553fa53c2ed0f401a3afbba094c69
Jimmy5541 <85048674+Jimmy5541@users.noreply.github.com>
2022-10-22 23:39:55 +0700

    Changed default callbacks to TensorBoard callback

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -9,10 +9,12 @@ from load_and_process import load_fer2013
 from load_and_process import preprocess_input
 from models.cnn import mini_XCEPTION
 from sklearn.model_selection import train_test_split
+import datetime
+import tensorflow as tf
 
 # parameters
 batch_size = 32
-num_epochs = 10000
+num_epochs = 10
 input_shape = (48, 48, 1)
 validation_split = .2
 verbose = 1
@@ -47,18 +49,21 @@ early_stop = EarlyStopping('val_loss', patience=patience)
 reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
                                   patience=int(patience/4), verbose=1)
 trained_models_path = base_path + '_mini_XCEPTION'
-model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+model_names = trained_models_path + '.{epoch:02d}-{val_accuracy:.2f}.hdf5'
 model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
                                                     save_best_only=True)
 callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr]
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 
 # loading dataset
 faces, emotions = load_fer2013()
 faces = preprocess_input(faces)
 num_samples, num_classes = emotions.shape
 xtrain, xtest,ytrain,ytest = train_test_split(faces, emotions,test_size=0.2,shuffle=True)
-model.fit_generator(data_generator.flow(xtrain, ytrain,
+model.fit(data_generator.flow(xtrain, ytrain,
                                             batch_size),
                         steps_per_epoch=len(xtrain) / batch_size,
-                        epochs=num_epochs, verbose=1, callbacks=callbacks,
+                        epochs=num_epochs, verbose=1, callbacks=[tensorboard_callback],
                         validation_data=(xtest,ytest))
